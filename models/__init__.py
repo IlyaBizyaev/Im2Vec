58e22553ed222525e487a20dac9b4621f7afd7e6
Ilya Bizyaev <bizyaev@zoho.com>
2021-05-07 13:32:41 +0400

    Drop unused alias

--- models/__init__.py
+++ models/__init__.py
@@ -4,10 +4,6 @@ from .vector_vae import VectorVAE
 from .vector_vae_nlayers import VectorVAEnLayers
 
 
-# Aliases
-VAE = VanillaVAE
-
-
 vae_models = {'VanillaVAE': VanillaVAE,
               'VectorVAE': VectorVAE,
               'VectorVAEnLayers': VectorVAEnLayers}
