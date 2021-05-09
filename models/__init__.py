884722ca356b60e8129713cb9fcbf21b7325098d
Ilya Bizyaev <bizyaev@zoho.com>
2021-05-09 15:41:22 +0400

    Drop the vanilla VAE

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,9 @@
 from .base import *
-from .vanilla_vae import *
 from .vector_vae import VectorVAE, raster_verbose, HIGH, LOW
 from .vector_vae_nlayers import VectorVAEnLayers
 
 
 VAE_MODELS = {
-    'VanillaVAE': VanillaVAE,
     'VectorVAE': VectorVAE,
     'VectorVAEnLayers': VectorVAEnLayers
 }
